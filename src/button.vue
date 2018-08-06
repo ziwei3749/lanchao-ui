7a2377f7807f661d26e8cfffbf87df5290b81caf
吕子威 <18500905537@163.com>
2018-08-06 18:04:23 +0800

    填写icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,28 @@
 <!-- button -->
 <template>
-    <button class="l-button">按钮</button>
+    <button class="l-button" :class="`icon-${iconPosition}`">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+        
+    </button>
 </template>
 
 <script>
 export default {
+    props: {
+        icon: {
+            type: String
+        },
+        iconPosition: {
+            type: String,
+            default: "left"
+        }
+    },
+
     data() {
         return {};
     },
@@ -20,6 +38,10 @@ export default {
 </script>
 <style lang="scss">
 .l-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     height: var(--button-height);
     font-size: var(--font-size);
     padding: 0 1em;
@@ -36,6 +58,25 @@ export default {
 
     &:focus {
         outline: none;
+    }
+
+    > .icon {
+        order: 1;
+        margin-right: .3em;
+    }
+    > .content {
+        order: 2;
+    }
+
+    &.icon-right {
+        > .icon {
+            order: 2;
+            margin-right: 0;
+            margin-left: .3em;
+        }
+        > .content {
+            order: 1;
+        }
     }
 }
 </style>
