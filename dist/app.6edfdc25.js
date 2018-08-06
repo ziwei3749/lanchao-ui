5f50ba51b827afd92a1933c4a001589d97f16ff3
吕子威 <18500905537@163.com>
2018-08-06 15:27:53 +0800

    .gitignore dist/和.cache

--- dist/app.6edfdc25.js
+++ dist/app.6edfdc25.js
@@ -10427,7 +10427,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '61153' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '62709' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
