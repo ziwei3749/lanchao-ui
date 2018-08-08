dcf8374599183aef803aa5821d49b4eb5beebad6
吕子威 <18500905537@163.com>
2018-08-08 11:13:21 +0800

    处理import语法问题后再次发布

---
+++ index.js
@@ -0,0 +1,6 @@
+// main里设置了我们npm包的入口是index.js
+import Button from "./src/button";
+import ButtonGroup from "./src/button-group";
+import Icon from "./src/icon";
+
+export { Button, ButtonGroup, Icon };
